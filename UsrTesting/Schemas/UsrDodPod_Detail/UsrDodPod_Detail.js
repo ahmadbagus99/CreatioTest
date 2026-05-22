f3f28f4d6bcec022ee2dd96f323814c85ae7283c
Ahmad Bagus <ahmad.99bagus@gmail.com>
2026-05-22 19:43:14 +0700

    Push section DodPod

---
+++ UsrTesting/Schemas/UsrDodPod_Detail/UsrDodPod_Detail.js
@@ -0,0 +1,8 @@
+define('UsrDodPod_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrDodPod',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
---
+++ UsrTesting/Schemas/UsrDodPod_FormPage/UsrDodPod_FormPage.js
@@ -0,0 +1,112 @@
+define("UsrDodPod_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "UsrName",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 1,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.UsrName",
+					"control": "$UsrName",
+					"labelPosition": "auto"
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "merge",
+				"name": "AttachmentList",
+				"values": {
+					"type": "crt.FileList",
+					"masterRecordColumnValue": "$Id",
+					"recordColumnName": "RecordId",
+					"layoutConfig": {
+						"colSpan": 2,
+						"column": 1,
+						"row": 1,
+						"rowSpan": 6
+					},
+					"items": "$AttachmentList",
+					"primaryColumnName": "AttachmentListDS_Id",
+					"columns": [
+						{
+							"id": "4bb2ba4f-7c25-4e33-b303-d4aff28fb25b",
+							"code": "AttachmentListDS_Name",
+							"caption": "#ResourceString(AttachmentListDS_Name)#",
+							"dataValueType": 28,
+							"width": 200
+						}
+					],
+					"viewType": "gallery",
+					"tileSize": "small"
+				},
+				"parentName": "AttachmentsTabContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "merge",
+				"name": "Feed",
+				"values": {
+					"type": "crt.Feed",
+					"feedType": "Record",
+					"primaryColumnValue": "$Id",
+					"cardState": "$CardState",
+					"dataSourceName": "PDS",
+					"entitySchemaName": "UsrDodPod"
+				},
+				"parentName": "FeedTabContainer",
+				"propertyName": "items",
+				"index": 0
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfig: /**SCHEMA_VIEW_MODEL_CONFIG*/{
+			"attributes": {
+				"UsrName": {
+					"modelConfig": {
+						"path": "PDS.UsrName"
+					}
+				},
+				"Id": {
+					"modelConfig": {
+						"path": "PDS.Id"
+					}
+				}
+			}
+		}/**SCHEMA_VIEW_MODEL_CONFIG*/,
+		modelConfig: /**SCHEMA_MODEL_CONFIG*/{
+			"dataSources": {
+				"PDS": {
+					"type": "crt.EntityDataSource",
+					"config": {
+						"entitySchemaName": "UsrDodPod"
+					},
+					"scope": "page"
+				},
+				"AttachmentListDS": {
+					"type": "crt.EntityDataSource",
+					"scope": "viewElement",
+					"config": {
+						"entitySchemaName": "SysFile",
+						"attributes": {
+							"Name": {
+								"path": "Name"
+							}
+						}
+					}
+				}
+			},
+			"primaryDataSourceName": "PDS"
+		}/**SCHEMA_MODEL_CONFIG*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
