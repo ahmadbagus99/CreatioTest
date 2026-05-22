81cf7551909fe834262249cec05f0d4e07dc16ac
Ahmad Bagus <ahmad.99bagus@gmail.com>
2026-05-22 19:36:07 +0700

    Push Section Testing

---
+++ UsrTesting/Schemas/UsrKing_Detail/UsrKing_Detail.js
@@ -0,0 +1,8 @@
+define('UsrKing_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrKing',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
