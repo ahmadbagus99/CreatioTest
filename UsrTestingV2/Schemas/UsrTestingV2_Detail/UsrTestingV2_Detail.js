84cc1dbb46795c4427661de9ab5acd99dc66cc12
Ahmad Bagus <ahmad.99bagus@gmail.com>
2026-05-24 11:53:26 +0700

    Update Test V2

---
+++ UsrTestingV2/Schemas/UsrTestingV2_Detail/UsrTestingV2_Detail.js
@@ -0,0 +1,8 @@
+define('UsrTestingV2_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrTestingV2',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
